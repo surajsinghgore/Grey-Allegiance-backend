72b742c758d27315d232e49de5d846033d4f89c8
suraj singh <surajthakurrs45@gmail.com>
2025-03-15 16:38:35 +0530

    removed

--- routes/requestQuote.router.js
+++ routes/requestQuote.router.js
@@ -2,12 +2,12 @@ import express from "express";
 
 import { createRequestQuote, getAllRequestQuotes, getPendingRequestQuotes, updateRequestQuoteStatus } from "../controller/requestQuote.controller.js";
 import { AdminVerifyMiddleware } from "../middlewares/AdminVerify.middleware.js";
-import { validateRequestQuote } from "../validators/requestQuote.validator.js";
+// import { validateRequestQuote } from "../validators/requestQuote.validator.js";
 
 
 const router = express.Router();
 
-router.post("/create", validateRequestQuote,createRequestQuote);
+router.post("/create",createRequestQuote);
 router.patch("/update-quote-status/:id", AdminVerifyMiddleware,updateRequestQuoteStatus);
 router.get("/pending", getPendingRequestQuotes);
 router.get("/all", getAllRequestQuotes);
