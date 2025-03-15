90d7ba2d90252ee26d32798e329acd3c4d250bbf
suraj singh <surajthakurrs45@gmail.com>
2025-03-15 16:25:27 +0530

    fix:fixing server error

--- routes/requestQuote.router.js
+++ routes/requestQuote.router.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { validateRequestQuote } from "../validators/requestQuote.validator.js";
+
 import { createRequestQuote, getAllRequestQuotes, getPendingRequestQuotes, updateRequestQuoteStatus } from "../controller/requestQuote.controller.js";
 import { AdminVerifyMiddleware } from "../middlewares/AdminVerify.middleware.js";
+import { validateRequestQuote } from "../validators/requestQuote.validator.js";
 
 
 const router = express.Router();
