561043fd77312c324a6d80e731127f53b02ee854
suraj singh <surajthakurrs45@gmail.com>
2025-03-19 16:17:39 +0530

    feat:allow new port for backend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { app } from "./app.js";
 const server = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173","https://grey-allegiance.vercel.app"],
   credentials: true, 
 };
 
