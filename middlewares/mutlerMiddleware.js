21595ea3287a753b8d6b1062145d3c9c70eb01fe
suraj singh <surajthakurrs45@gmail.com>
2025-03-23 14:10:25 +0530

    multer  code modified

--- middlewares/mutlerMiddleware.js
+++ middlewares/mutlerMiddleware.js
@@ -1,18 +1,10 @@
 import multer from "multer";
 import path from "path";
-import fs from "fs";
 
-// Define the temp directory inside greyAlligence
-const tempDir = path.join(process.cwd(), "greyallegiancebackend", "tmp");
-
-// Ensure the temp directory exists
-if (!fs.existsSync(tempDir)) {
-  fs.mkdirSync(tempDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, tempDir); // Store files in greyAlligence/temp
+    cb(null, "/tmp"); 
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
@@ -20,5 +12,8 @@ const storage = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage });
+export const upload = multer({
+  storage,
+});
+
 
