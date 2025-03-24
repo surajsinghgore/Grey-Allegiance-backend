f341107d5e73447eb1de553fba0a9cb4341eede7
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2025-03-24 19:58:42 +0530

    email frotend url changed

--- utils/EmailTemplate/UserTemplate.js
+++ utils/EmailTemplate/UserTemplate.js
@@ -2,7 +2,7 @@ export const generateForgotPasswordEmail = (resetToken) => {
     return `
         <h2>Password Reset Request</h2>
         <p>You requested to reset your password. Please click the link below to reset your password:</p>
-        <p><a href="${process.env.CLIENT_URL}/reset-password/${resetToken}">Reset Your Password</a></p>
+        <p><a href="${process.env.CLIENT_URL}/auth/reset-password/${resetToken}">Reset Your Password</a></p>
         <p>This link will expire in 1 hour. If you did not request a password reset, please ignore this email.</p>
     `;
 };
